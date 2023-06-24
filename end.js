19f91558c23deb610a970cf559513628eecaddce
Sugan T <t.suganeeshwaran@gmail.com>
2023-06-24 15:10:13 +0530

    localStorage in end page

--- end.js
+++ end.js
@@ -6,10 +6,10 @@ const mostRecentScore = localStorage.getItem("mostRecentScore");
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
-  saveScoreBtn.disabled = !username.ariaValueMax;
+  saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = (e) => {
-  console.log("clicked the save button");
   e.preventDefault();
+  console.log("clicked the save button");
 };
